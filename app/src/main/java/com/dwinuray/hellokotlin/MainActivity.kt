e24caf6e812b529dc853742621cec53d656f77ff
Dwi Nur Cahyo <47982352+dwinuray@users.noreply.github.com>
2020-11-26 06:02:54 +0700

    Testing initialize variable and find component button by id

--- app/src/main/java/com/dwinuray/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/dwinuray/hellokotlin/MainActivity.kt
@@ -2,11 +2,19 @@ package com.dwinuray.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button = findViewById(R.id.button) as Button
+        button.setOnClickListener({
+
+            Toast.makeText(this@MainActivity, "Okei clicked", Toast.LENGTH_SHORT).show();
+        })
     }
 }
